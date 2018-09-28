f8e97d6ab399bdc90a20e390601511b4b79086d4
Isabelle <belletay@stanford.edu>
2018-09-28 14:04:55 -0700

    initial commit of pset0.R

---
+++ pset0.R
@@ -0,0 +1,11 @@
+---
+title: "pset0.R"
+author: "Isabelle"
+date: "28/09/2018"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+hello this is a sentence.
